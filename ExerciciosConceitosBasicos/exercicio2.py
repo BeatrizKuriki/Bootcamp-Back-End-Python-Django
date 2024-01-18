c8e19a89a63ca9a70a9564d51a25a125e34c1cb1
Liane Rafaelle <141535641+lianerafaelle@users.noreply.github.com>
2024-01-18 18:20:34 -0300

    exercicio2.py

---
+++ ExerciciosConceitosBasicos/exercicio2.py
@@ -0,0 +1,12 @@
+solução do exercicio . py
+
+#solicitar ao usuario o ano de nascimento
+ano_nascimento = int(input('informe o ano do seu nascimento '))
+
+#calculara idade atual 
+ano_atual= 2024
+#pode ser ajustado para o ano atual
+idade=ano_atual - ano_nascimento
+ 
+#im primir a idade atual
+print('Sua idade atual é:',idade,'anos')
