6707d34bd1bf2c8176366b3e01fc64650e3cae88
Beatriz Alencar Kuriki <beatriz.alencar@sesatech.com.br>
2024-01-18 10:11:12 -0300

    feat: exercício 5-Funções (beatriz)

---
+++ ExerciciosFuncoes/exercicio5.py
@@ -0,0 +1,16 @@
+# Crie uma função chamada contar_vogais que recebe uma string
+# como parâmetro. Implemente a lógica para contar o número de vogais
+# na string e retorne o total de vogais. Solicite ao usuário para inserir uma
+# frase e utilize a função para contar as vogais.
+
+import re
+
+def conta_vogais(texto):
+    padrao = r'[aeiouáéíóúàèìòùãõâêîôûäëïöü]'   
+    vogais_encontradas = re.findall(padrao, texto, re.IGNORECASE)
+    return len(vogais_encontradas)
+
+frase = input('Digite seu texto aqui:')
+print("Total de vogais:", conta_vogais(frase))
+
+
