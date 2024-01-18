19e2cfb1d4d6d4bd0becf820f50b823f4bfcbedf
Cíntia Brito <cintiahsb@hotmail.com>
2024-01-18 19:56:53 -0300

    ex 9 tomada de decisão

--- ExerciciosTomadaDecisãoRepeticao/exercicio9.py
+++ ExerciciosTomadaDecisãoRepeticao/exercicio9.py
@@ -7,3 +7,21 @@
 # informar o valor zero. Certifique-se de incluir validações para garantir que
 # apenas números positivos sejam considerados na contagem e cálculos.
 
+
+num = int(input("Digite um número positivo: "))
+pares = []
+impares = []
+
+while num != 0:
+    if num < 0:
+        print("Esse número é negativo! Tente outro!")
+        num = int(input("Digite um número: "))
+    elif num % 2 == 0:
+        pares.append(num)
+        num = int(input("Digite um número: "))
+    else :
+        impares.append(num)
+        num = int(input("Digite um número: "))
+
+print("Quantidade de Números Pares", len(pares))
+print("Quantidade de Números Ímpares", len(impares))
